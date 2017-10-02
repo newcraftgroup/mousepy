2a7e9adc7b99aab0d8f705c54280338316a94204
Cédric Le Varlet <cedriclv@gmail.com>
2017-10-02 15:38:06 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nci-omniture-mouseflow",
-    version="0.0.4",
-    description="Forwards adobe omniture segments to mouseflow",
-    url="https://github.com/conversioncompany/nci-omniture-mouseflow",
+    name="nci-python-mouseflow",
+    version="0.0.5",
+    description="Client library for Mouseflow's API",
+    url="https://github.com/conversioncompany/nci-python-mouseflow",
     author='Newcraft',
     author_email='cedric.le.varlet@newcraftgroup.com',
     license='',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    keywords='development adobe omniture',
-    packages=find_packages(exclude=['config', 'manage', 'docs', 'tests*', 'export']),
-    install_requires=["pandas", "texttable", "requests"],
+    keywords='api mouseflow',
+    packages=find_packages(exclude=['docs']),
+    install_requires=["pandas", "requests"],
 )
