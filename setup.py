c1cf844a2fc672a6fbf1146cd3a80b01cf485ed8
Cédric Le Varlet <cedriclv@gmail.com>
2017-10-18 13:55:32 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nci-python-mouseflow",
-    version="0.0.5",
+    version="0.0.6",
     description="Client library for Mouseflow's API",
     url="https://github.com/conversioncompany/nci-python-mouseflow",
     author='Newcraft',
